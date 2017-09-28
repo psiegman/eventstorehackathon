6f96937ef24738440687f7569da170d1368f9e8d
Paul Siegmann <paul.siegmann+github@gmail.com>
2017-09-28 16:26:50 +0200

    remove jpa dependencies from event module

--- api/src/main/java/nl/gerimedica/axoniqhack/gmhack/events/domain/GeoLocation.java
+++ api/src/main/java/nl/gerimedica/axoniqhack/gmhack/events/domain/GeoLocation.java
@@ -4,17 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GeoLocation {
 
-	@Column
 	double latitude;
 
-	@Column
 	double longitude;
 }
