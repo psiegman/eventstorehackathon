6f356e6e19a01d02c9dc637e63c1adc14b4a3546
Arno Koehler <arno.koehler@gmail.com>
2017-09-28 17:12:58 +0200

    lombok

--- api/src/main/java/nl/gerimedica/axoniqhack/gmhack/events/IncidentResolvedEvent.java
+++ api/src/main/java/nl/gerimedica/axoniqhack/gmhack/events/IncidentResolvedEvent.java
@@ -1,11 +1,17 @@
 package nl.gerimedica.axoniqhack.gmhack.events;
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import nl.gerimedica.axoniqhack.gmhack.events.domain.Resolution;
 
+@Data
+@AllArgsConstructor
+@Builder
 public class IncidentResolvedEvent {
 
-    private String id;
+    private Long id;
     private Resolution resolution;
     private LocalDateTime resolutionTime;
 }
