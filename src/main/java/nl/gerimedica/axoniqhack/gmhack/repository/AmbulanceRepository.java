6f70d6067682865aaccfd6b6fa2acdfb0be3ef4f
Paul Siegmann <paul.siegmann+github@gmail.com>
2017-09-28 14:31:39 +0200

    add ambulances

---
+++ src/main/java/nl/gerimedica/axoniqhack/gmhack/repository/AmbulanceRepository.java
@@ -0,0 +1,10 @@
+package nl.gerimedica.axoniqhack.gmhack.repository;
+
+import nl.gerimedica.axoniqhack.gmhack.domain.Ambulance;
+import nl.gerimedica.axoniqhack.gmhack.domain.Station;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AmbulanceRepository extends CrudRepository<Ambulance, Long>{
+
+
+}
