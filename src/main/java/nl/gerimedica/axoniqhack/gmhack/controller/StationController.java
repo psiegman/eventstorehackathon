bbdd027becfd7aa578cfe9899245faa328f3fc87
Paul Siegmann <paul.siegmann+github@gmail.com>
2017-09-28 14:15:44 +0200

    add working station entity

--- src/main/java/nl/gerimedica/axoniqhack/gmhack/controller/StationController.java
+++ src/main/java/nl/gerimedica/axoniqhack/gmhack/controller/StationController.java
@@ -3,20 +3,26 @@ package nl.gerimedica.axoniqhack.gmhack.controller;
 import nl.gerimedica.axoniqhack.gmhack.domain.Station;
 import nl.gerimedica.axoniqhack.gmhack.repository.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-@RequestMapping
+@Controller
+@RequestMapping("/station")
 public class StationController {
 
 	@Autowired
 	private StationRepository stationRepository;
 
-	List<Station> getAll() {
-		return StreamSupport.stream(stationRepository.findAll().spliterator(), false).collect(Collectors.toList());
+	@RequestMapping("/")
+	public String getAllStations(Model model) {
+		List<Station> stations = StreamSupport.stream(stationRepository.findAll().spliterator(), false).collect(Collectors.toList());
+		model.addAttribute("station", stations);
+		return "station/index";
 	}
 }
 
