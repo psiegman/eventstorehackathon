31a52f9a4be891555c444c0e06b53d962377c7a4
Paul Siegmann <paul.siegmann+github@gmail.com>
2017-09-28 13:25:10 +0200

    initial commit

---
+++ src/main/java/nl/gerimedica/axoniqhack/gmhack/GmhackApplication.java
@@ -0,0 +1,13 @@
+package nl.gerimedica.axoniqhack.gmhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmhackApplication.class, args);
+	}
+}
